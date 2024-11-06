229415fc770873fbccdb053e73229cc0d36efc3b
Sinan Öksüz <98950852+sinanoksz@users.noreply.github.com>
2024-11-06 01:22:44 +0100

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,24 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
+      const { text, locale } = req.body;
       
+      if (text === undefined || locale === undefined) {
+        return res.json({ error: 'Required field(s) missing' });
+      }
+      
+      if (text === '') {
+        return res.json({ error: 'No text to translate' });
+      }
+
+      if (!['american-to-british', 'british-to-american'].includes(locale)) {
+        return res.json({ error: 'Invalid value for locale field' });
+      }
+
+      const result = translator.translate(text, locale);
+      if (result.error) {
+        return res.json({ error: result.error });
+      }
+      res.json(result);
     });
 };
